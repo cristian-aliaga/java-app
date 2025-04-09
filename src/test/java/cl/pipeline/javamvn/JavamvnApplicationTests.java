ecd97b7f9e8a590d9fae108fc509b22e0bf9104d
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2025-04-08 22:03:51 -0400

    First Commit: Pipelina, Dockerfile and Deploy file

---
+++ src/test/java/cl/pipeline/javamvn/JavamvnApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.pipeline.javamvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
